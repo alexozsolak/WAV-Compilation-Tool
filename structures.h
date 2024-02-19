e801fc1b242783ecae029061e3f1af2cd6f8f8ca
alexozsolak <160542800+alexozsolak@users.noreply.github.com>
2024-02-19 16:16:34 -0500

    Update structures.h

--- structures.h
+++ structures.h
@@ -1,5 +1,3 @@
-// BMP-related data types based on Microsoft's own
-
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -10,18 +8,10 @@ typedef struct
     bool output;
 } song;
 
-// These data types are essentially aliases for C/C++ primitive data types.
-// Adapted from http://msdn.microsoft.com/en-us/library/cc230309.aspx.
-// See https://en.wikipedia.org/wiki/C_data_types#stdint.h for more on stdint.h.
-
 typedef uint8_t   BYTE;
 typedef uint16_t  WORD;
 typedef int16_t   SAMPLE;
 typedef uint32_t  DWORD;
-
-// The WAVHEADER structure contains information about the type, size,
-// and layout of a file that contains audio samples.
-// Adapted from http://soundfile.sapp.org/doc/WaveFormat/.
 
 typedef struct
 {
